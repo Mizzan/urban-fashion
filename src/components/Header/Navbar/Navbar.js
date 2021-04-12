4ffa20dcf7cc9e7b4efb29c6b2a4e109c51f072f
Mizanur Rahman <mizzanur007@gmail.com>
2021-04-12 17:38:05 +0600

    based project setup finished

---
+++ src/components/Header/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../../../images/logoHeader.png';
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <nav>
+        <div className="brand">
+          <img src={logo} alt="The urban fashion logo" />
+        </div>
+        <div className="menu-list">
+          <a href="#">Home</a>
+          <a href="#">Shop</a>
+          <a href="#">Product</a>
+          <a href="#">Page</a>
+          <a href="#">Blog</a>
+        </div>
+        <div className="nav-right">
+          <div className="search-field"></div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
